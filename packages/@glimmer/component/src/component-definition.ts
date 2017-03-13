39f5376b52f99ae5d6e8ddbafde23da113805cd1
Tom Dale <tom@tomdale.net>
2017-03-12 22:22:01 -0400

    Store compiled layout on ComponentDefinition

--- packages/@glimmer/component/src/component-definition.ts
+++ packages/@glimmer/component/src/component-definition.ts
@@ -1,6 +1,7 @@
 import {
   ComponentClass,
-  ComponentDefinition as GlimmerComponentDefinition
+  ComponentDefinition as GlimmerComponentDefinition,
+  CompiledDynamicProgram
 } from '@glimmer/runtime';
 import ComponentManager from './component-manager';
 import Component from './component';
@@ -9,11 +10,13 @@ import ComponentFactory from './component-factory';
 export default class ComponentDefinition extends GlimmerComponentDefinition<Component> {
   public name: string;
   public manager: ComponentManager;
+  public layout: CompiledDynamicProgram;
   public ComponentClass: ComponentClass;
   public componentFactory: ComponentFactory;
 
-  constructor(name: string, manager: ComponentManager, ComponentClass: ComponentClass) {
+  constructor(name: string, manager: ComponentManager, layout: CompiledDynamicProgram, ComponentClass: ComponentClass) {
     super(name, manager, ComponentClass);
+    this.layout = layout;
     this.componentFactory = new ComponentFactory(ComponentClass);
   }
 }
