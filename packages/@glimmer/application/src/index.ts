f3e031ee2438e13220e7549aea93ebea841f715d
Tom Dale <tom@tomdale.net>
2017-03-20 14:45:22 -0400

    Export TemplateMeta

--- packages/@glimmer/application/src/index.ts
+++ packages/@glimmer/application/src/index.ts
@@ -1,2 +1,3 @@
 export { default, ApplicationOptions, Initializer } from './application';
 export { default as Environment } from './environment';
+export { default as TemplateMeta } from './template-meta';
