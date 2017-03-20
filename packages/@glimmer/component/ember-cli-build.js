f2dce45541ef9ae8a3ce7081c6ff5cee30435aad
Tom Dale <tom@tomdale.net>
2017-03-20 14:46:57 -0400

    Brocfile.js -> ember-cli-build.js

---
+++ packages/@glimmer/component/ember-cli-build.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const build = require('@glimmer/build');
+const packageDist = require('@glimmer/build/lib/package-dist');
+const buildVendorPackage = require('@glimmer/build/lib/build-vendor-package');
+const funnel = require('broccoli-funnel');
+const path = require('path');
+
+module.exports = function() {
+  let vendorTrees = [
+    '@glimmer/application',
+    '@glimmer/resolver',
+    '@glimmer/compiler',
+    '@glimmer/di',
+    '@glimmer/object-reference',
+    '@glimmer/reference',
+    '@glimmer/runtime',
+    '@glimmer/syntax',
+    '@glimmer/util',
+    '@glimmer/wire-format'
+  ].map(packageDist);
+
+  vendorTrees.push(buildVendorPackage('simple-html-tokenizer'));
+  vendorTrees.push(funnel(path.dirname(require.resolve('handlebars/package')), {
+      include: ['dist/handlebars.amd.js'] }));
+
+  return build({ vendorTrees });
+}
