cabaff1f982ecd2e33a789d5761cc7cd51fb9ce6
Robbie Pitts <me@robbie-pitts.com>
2017-03-26 14:40:32 -0400

    Add web component support

--- packages/@glimmer/blueprint/files/src/index.ts
+++ packages/@glimmer/blueprint/files/src/index.ts
@@ -1,5 +1,6 @@
 import App from './main';
 import { ComponentManager, setPropertyDidChange } from '@glimmer/component';
+import initializeCustomElements from '@glimmer/web-component';
 
 const app = new App();
 const containerElement = document.getElementById('app');
@@ -17,3 +18,5 @@ app.registerInitializer({
 app.boot();
 
 app.renderComponent('<%= component %>', containerElement, null);
+
+initializeCustomElements(app, ['<%= component %>']);
