88cf06c7faf029afcfc025bd1c1826e581b9fa3c
Tom Dale <tom@tomdale.net>
2017-08-15 17:17:14 -0400

    Fix Simple DOM type issue

--- packages/@glimmer/component/test/test-helpers/test-app.ts
+++ packages/@glimmer/component/test/test-helpers/test-app.ts
@@ -3,8 +3,12 @@ import { Simple } from '@glimmer/interfaces';
 import { AppBuilder, AppBuilderOptions } from '@glimmer/application-test-helpers';
 import { ComponentManager } from '../..';
 
+export interface TestElement extends Simple.Element {
+  textContent: string;
+}
+
 export class TestApplication extends Application {
-  rootElement: Simple.Element;
+  rootElement: TestElement;
 }
 
 export default function buildApp(appName = 'test-app', options: AppBuilderOptions = {}): AppBuilder {
