8ccba8cae06af9f5bb6222f8eeae508d71910728
Chris Garrett <cgarrett@linkedin.com>
2020-01-30 19:34:20 -0800

    remove @glimmer/application logic from ember-addon

--- packages/@glimmer/component/ember-addon-main.js
+++ packages/@glimmer/component/ember-addon-main.js
@@ -4,19 +4,5 @@ const writeFile = require('broccoli-file-creator');
 const MergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
-  name: '@glimmer/component',
-
-  included(includer) {
-    let pkg = 'pkg' in includer ? includer.pkg : includer.project.pkg;
-
-    if (
-      (pkg.dependencies && '@glimmer/application' in pkg.dependencies) ||
-      (pkg.devDependencies && '@glimmer/application' in pkg.devDependencies)
-    ) {
-      // this is a Glimmer application, don't include nested addons or run addon code
-      return;
-    }
-
-    this._super.included.apply(this, arguments);
-  },
+  name: '@glimmer/component'
 };
