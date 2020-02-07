234e51b349c5ce34b53800ad969099fcdd2d8de1
Chris Garrett <cgarrett@linkedin.com>
2020-02-06 17:10:22 -0800

    add clean script

---
+++ bin/clean.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const fs = require('fs-extra');
+const { promisify } = require('util');
+
+const glob = promisify(require('glob'));
+
+(async function() {
+  await remove(await glob('./packages/@glimmer/*/dist'));
+
+  console.log('\n\nDone');
+})();
+
+function remove(paths) {
+  return each(paths, pathOfDir => {
+    console.log(`Removing ${pathOfDir}`);
+    return fs.remove(pathOfDir);
+  });
+}
+
+function each(paths, cb) {
+  return Promise.all(paths.map(cb));
+}
