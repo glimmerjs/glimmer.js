96fdb5db6e59c4cefbfee0018bd398a129ff56a8
Chris Garrett <cgarrett@linkedin.com>
2020-08-28 09:28:30 -0700

    update test fixtures

--- packages/babel-plugins/@glimmer/babel-plugin-strict-template-precompile/test/fixtures-options/precompile/ast-transform/output.js
+++ packages/babel-plugins/@glimmer/babel-plugin-strict-template-precompile/test/fixtures-options/precompile/ast-transform/output.js
@@ -1,6 +1,6 @@
 ({
-  id: "k5+y7pkD",
-  block: "{\"symbols\":[],\"statements\":[[9,\"h1\",true],[10],[1,1,0,0,\"Hello world\"],[11]],\"hasEval\":false,\"upvars\":[]}",
+  id: "iLYq0mJl",
+  block: "{\"symbols\":[],\"statements\":[[10,\"h1\"],[12],[2,\"Hello world\"],[13]],\"hasEval\":false,\"upvars\":[]}",
   meta: {
     scope: () => ({})
   }
