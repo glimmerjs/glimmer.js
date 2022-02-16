25d538487d1cf703f32063498907bc3089f2bd18
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-02-16 13:10:56 -0500

    Upgrade qunit setup for @glimmer/component

--- packages/@glimmer/component/test/ember/test-helper.js
+++ packages/@glimmer/component/test/ember/test-helper.js
@@ -2,7 +2,10 @@ import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
-import 'qunit-dom';
+import { setup } from 'qunit-dom';
+import * as QUnit from 'qunit';
+
+setup(QUnit.assert);
 
 setApplication(Application.create(config.APP));
 
