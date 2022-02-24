fe0d39d7d34f60b111853a4dd80fc4931ae3cac7
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-02-24 09:07:25 -0500

    Re-remove support for 3.16 and 3.12 because there is *no way* they actually work, considering the destroyable apis weren't added until 3.22

--- packages/@glimmer/component/config/ember-try.js
+++ packages/@glimmer/component/config/ember-try.js
@@ -12,22 +12,6 @@ module.exports = function () {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-3.12',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.12.0',
-            },
-          },
-        },
-        {
-          name: 'ember-lts-3.16',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.16.0',
-            },
-          },
-        },
-        {
           name: 'ember-lts-3.24',
           npm: {
             devDependencies: {
