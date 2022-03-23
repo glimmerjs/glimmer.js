d9f1b4e538b2ec726f3ccf7169150e215076bb85
Chris Krycho <hello@chriskrycho.com>
2022-03-23 10:34:06 -0600

    Introduce Component `Signature` (RFC 0748)
    Adds backwards-compatible support for component `Signature`s per [Ember
    RFC #0748][rfc]. The public API of `Component` is now *more* permissive
    than it was previously, because it changes the type parameter to be an
    unconstrained generic `<S>` (for "signature") which can accept *either*
    the existing `Args` types *or* a new `Signature` which includes `Args`
    and adds `Blocks` and `Element`.
    
    [rfc]: https://github.com/emberjs/rfcs/pull/748
    
    The `Args` part of the new signature work exactly like the old
    args-only type did. The `Blocks` and `Element` parts of a signature are
    inert from the perspective of TypeScript users who are not yet using
    [Glint][glint], but Glint users will benefit directly once Glint
    releases an update which can requires a version of `@glimmer/component`
    incorporating this change.
    
    [glint]: https://github.com/typed-ember/glint
    
    Since this change is backwards compatible, we can deprecate the
    non-`Signature` form at a later time when we are ready for a 2.0
    release.
    
    To validate these changes, with the relatively complicated type
    machinery they require under the hood, this also introduces the
    `expect-type` type testing library, rewrites the existing type tests
    using it, and introduces new type tests for all supported forms of the
    `Signature`.
    
    (cherry picked from commit 967d028de585fd0175c72af1ed96795b0018895e)

--- packages/@glimmer/component/addon/index.ts
+++ packages/@glimmer/component/addon/index.ts
@@ -1,10 +1,25 @@
 import { DEBUG } from '@glimmer/env';
-import ApplicationInstance from '@ember/application/instance';
+import type ApplicationInstance from '@ember/application/instance';
 import { setComponentManager } from '@ember/component';
 import { gte } from 'ember-compatibility-helpers';
 
+// Hax because the old version of `@types/ember__component` the `1.x` branch
+// uses does not provide any types for `setComponentManager` *and* because we
+// are using a very old version of `setComponentManager` for versions before
+// Ember 3.8.
+declare module '@ember/component' {
+  // The modern version.
+  export function setComponentManager<T extends object>(
+    factory: (owner: ApplicationInstance) => GlimmerComponentManager,
+    componentClass: T
+  ): T;
+
+  // The pre-3.8 version.
+  export function setComponentManager<T extends object>(name: string, componentClass: T): T;
+}
+
 import GlimmerComponentManager from './-private/ember-component-manager';
-import GlimmerComponentBase from './-private/component';
+import GlimmerComponentBase, { Args } from './-private/component';
 
 let GlimmerComponent = GlimmerComponentBase;
 
@@ -13,15 +28,19 @@ if (DEBUG) {
 
   // TODO: Add GlimmerComponent API docs link to these messages once API docs are live
   function throwMethodUseError(methodName: string) {
-    throw new Error(`You attempted to define the '${methodName}' method on a Glimmer Component, but that lifecycle hook does not exist in Ember.js applications, it only exists in Glimmer.js apps. You can rename this method, and you can trigger it using a modifier such as {{did-insert}} from '@ember/render-modifiers': https://github.com/emberjs/ember-render-modifiers.`);
+    throw new Error(
+      `You attempted to define the '${methodName}' method on a Glimmer Component, but that lifecycle hook does not exist in Ember.js applications, it only exists in Glimmer.js apps. You can rename this method, and you can trigger it using a modifier such as {{did-insert}} from '@ember/render-modifiers': https://github.com/emberjs/ember-render-modifiers.`
+    );
   }
 
   function throwPropertyUseError(propertyName: string) {
-    throw new Error(`You attempted to access the '${propertyName}' property on a Glimmer Component, but that property does not exist in Ember.js applications, it only exists in Glimmer.js apps. You define a class field with the same name on your component class and it will overwrite this error message, but it will not be used by the framework.`);
+    throw new Error(
+      `You attempted to access the '${propertyName}' property on a Glimmer Component, but that property does not exist in Ember.js applications, it only exists in Glimmer.js apps. You define a class field with the same name on your component class and it will overwrite this error message, but it will not be used by the framework.`
+    );
   }
 
-  GlimmerComponent = class GlimmerDebugComponent<T> extends GlimmerComponent<T> {
-    constructor(owner: unknown, args: T) {
+  GlimmerComponent = class GlimmerDebugComponent<S> extends GlimmerComponent<S> {
+    constructor(owner: unknown, args: Args<S>) {
       super(owner, args);
 
       if (typeof this['didInsertElement'] === 'function') {
@@ -34,9 +53,13 @@ if (DEBUG) {
     }
   };
 
-  let proto = GlimmerComponent.prototype as any;
+  let proto = GlimmerComponent.prototype;
 
-  function defineErrorProp(proto, key, getterMethod) {
+  function defineErrorProp(
+    proto: GlimmerComponentBase,
+    key: string,
+    getterMethod: (key: string) => unknown
+  ) {
     Object.defineProperty(proto, key, {
       get: () => getterMethod(key),
       set(value) {
--- test/types/component-test.ts
+++ test/types/component-test.ts
@@ -1,62 +1,96 @@
+import { expectTypeOf } from 'expect-type';
 import * as gc from '@glimmer/component';
-import { hasExactKeys } from './utils';
 
 const Component = gc.default;
 
-hasExactKeys<{
-  default: unknown,
-}>()(gc);
-
-// $ExpectType typeof Component
-gc.default;
+expectTypeOf(gc).toHaveProperty('default');
+expectTypeOf(gc.default).toEqualTypeOf<typeof Component>();
 
 type Args = {
   foo: number;
 };
 
-let component = new Component<Args>({}, { foo: 123 });
+const componentWithLegacyArgs = new Component<Args>({}, { foo: 123 });
+expectTypeOf(componentWithLegacyArgs).toHaveProperty('args');
+expectTypeOf(componentWithLegacyArgs).toHaveProperty('isDestroying');
+expectTypeOf(componentWithLegacyArgs).toHaveProperty('isDestroyed');
+expectTypeOf(componentWithLegacyArgs).toHaveProperty('willDestroy');
+expectTypeOf(componentWithLegacyArgs.args).toEqualTypeOf<Readonly<Args>>();
+expectTypeOf(componentWithLegacyArgs.isDestroying).toEqualTypeOf<boolean>();
+expectTypeOf(componentWithLegacyArgs.isDestroyed).toEqualTypeOf<boolean>();
+expectTypeOf(componentWithLegacyArgs.willDestroy).toEqualTypeOf<() => void>();
 
-hasExactKeys<{
-  args: unknown,
-  bounds: {
-    firstNode: unknown,
-    lastNode: unknown,
-  },
-  debugName: unknown,
-  didInsertElement: unknown,
-  didUpdate: unknown,
-  element: unknown,
-  isDestroying: unknown,
-  isDestroyed: unknown,
-  willDestroy: unknown,
-}>()(component);
+interface ArgsOnly {
+  Args: Args;
+}
 
-// $ExpectType Args
-component.args;
+const componentWithArgsOnly = new Component<ArgsOnly>({}, { foo: 123 });
+expectTypeOf(componentWithArgsOnly.args).toEqualTypeOf<Readonly<Args>>();
 
-// $ExpectType Bounds
-component.bounds;
+interface ElementOnly {
+  Element: HTMLParagraphElement;
+}
 
-// $ExpectType string
-component.debugName;
+const componentWithElOnly = new Component<ElementOnly>({}, {});
 
-// $ExpectType () => void
-component.didInsertElement;
+// We cannot check on toEqualTypeOf here b/c EmptyObject is intentionally not
+// public.
+expectTypeOf(componentWithElOnly.args).toMatchTypeOf<Readonly<{}>>();
 
-// $ExpectType () => void
-component.didUpdate;
+interface Blocks {
+  default: [name: string];
+  inverse: [];
+}
 
-// $ExpectType HTMLElement
-component.element;
+interface BlockOnlySig {
+  Blocks: Blocks;
+}
 
-// $ExpectType boolean
-component.isDestroying;
+const componentWithBlockOnly = new Component<BlockOnlySig>({}, {});
 
-// $ExpectType boolean
-component.isDestroyed;
+// We cannot check on toEqualTypeOf here b/c EmptyObject is intentionally not
+// public.
+expectTypeOf(componentWithBlockOnly.args).toMatchTypeOf<Readonly<{}>>();
 
-// $ExpectType () => void
-component.willDestroy;
+interface ArgsAndBlocks {
+  Args: Args;
+  Blocks: Blocks;
+}
 
-// $ExpectError
-component.args.bar = 123;
+const componentwithArgsAndBlocks = new Component<ArgsAndBlocks>({}, { foo: 123 });
+expectTypeOf(componentwithArgsAndBlocks.args).toEqualTypeOf<Readonly<Args>>();
+
+interface ArgsAndEl {
+  Args: Args;
+  Element: HTMLParagraphElement;
+}
+
+const componentwithArgsAndEl = new Component<ArgsAndEl>({}, { foo: 123 });
+expectTypeOf(componentwithArgsAndEl.args).toEqualTypeOf<Readonly<Args>>();
+
+interface FullShortSig {
+  Args: Args;
+  Element: HTMLParagraphElement;
+  Blocks: Blocks;
+}
+
+const componentWithFullShortSig = new Component<FullShortSig>({}, { foo: 123 });
+expectTypeOf(componentWithFullShortSig.args).toEqualTypeOf<Readonly<Args>>();
+
+interface FullLongSig {
+  Args: {
+    Named: Args;
+    Positional: [];
+  };
+  Element: HTMLParagraphElement;
+  Blocks: {
+    default: {
+      Params: {
+        Positional: [name: string];
+      };
+    };
+  };
+}
+
+const componentWithFullSig = new Component<FullLongSig>({}, { foo: 123 });
+expectTypeOf(componentWithFullSig.args).toEqualTypeOf<Readonly<Args>>();
